7960db0348b8c3d9401da93cca1e5d982028db17
Hamid Semix <semix.hamidouh@gmail.com>
2022-11-10 18:35:42 +0300

    Add vue for the ui

--- resources/views/templates/welcome.php
+++
@@ -1,7 +0,0 @@
-<div class="flex-center position full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            <?=$this->getAppName()?>, the only php mvvm framework
-        </div>
-    </div>
-</div>
